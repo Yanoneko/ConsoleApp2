6455d451ad93227c1883d3c04a8fddc7f016b5f9
Yanoneko <madam.marina2006@mail.ru>
2025-03-04 15:25:48 +0300

    добавление кода

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,6 +10,33 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            string x = getFigure();
+            Console.WriteLine(x);
+            Console.WriteLine("работу выполнила Петрова");
+            Console.ReadKey();
+        }
+        static string getFigure() //доп. метод
+        {
+            double a, b, c, sqrt;
+            Console.Write("a = ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("b = ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("c = ");
+            c = Convert.ToInt32(Console.ReadLine());
+            sqrt = Math.Sqrt(Math.Pow(b, 2) - 4 * a * c);
+            if (sqrt < 0)
+            {
+                return "корней нет"; //возвращение значения и выход из метода
+            }
+            else if (sqrt == 0)
+            {
+                return "1 корень"; //возвращение значения и выход из метода
+            }
+            else
+            {
+                return "2 корня"; //возвращение значения и выход из метода
+            }
         }
     }
 }
